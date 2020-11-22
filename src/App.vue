87f67f57713fb18860c554fbc4ac8d002c4410b5
apple <apple@appledeMacBook-Air-2.local>
2020-11-22 16:20:27 +0800

    docker部署以及nginx配置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="(item, index) in users" :key="index">{{item.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted () {
+    axios.get('/api/users').then((res) => {
+      this.users = res.data
+    })
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
